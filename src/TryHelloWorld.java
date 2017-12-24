74b0f3464e77dd641ebf1226ecccc0714aa055c6
mgh3326 <mgh3326@naver.com>
2017-12-24 20:58:50 +0900

    재귀함수 사용

--- src/TryHelloWorld.java
+++ src/TryHelloWorld.java
@@ -1,25 +1,39 @@
 import java.util.Arrays;
 
-class TryHelloWorld {
+class TryHelloWorld {//재귀함수 사용
     public int[] gcdlcm(int a, int b) {
         int[] answer = new int[2];
-        int z;
-        int x = a;
-        int y = b;
-        while (true) {
 
-            z = x % y;
-            if (0 == z)
-                break;
-
-            x = y;
-            y = z;
-
-        }
-        answer[0]=y;
-        answer[1]=(a*b/y);
+        answer[0] = gcd(a,b);
+        answer[1] = (a*b)/answer[0];
         return answer;
     }
+
+    public static int gcd(int p, int q)
+    {
+        if (q == 0) return p;
+        return gcd(q, p%q);
+    }
+//class TryHelloWorld { //유클리드 호제법
+//    public int[] gcdlcm(int a, int b) {
+//        int[] answer = new int[2];
+//        int z;
+//        int x = a;
+//        int y = b;
+//        while (true) {
+//
+//            z = x % y;
+//            if (0 == z)
+//                break;
+//
+//            x = y;
+//            y = z;
+//
+//        }
+//        answer[0]=y;
+//        answer[1]=(a*b/y);
+//        return answer;
+//    }
 
     // 아래는 테스트로 출력해 보기 위한 코드입니다.
     public static void main(String[] args) {
